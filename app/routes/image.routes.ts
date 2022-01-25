84018cb313f371c3b32b60a5a7bfbdc7ffa0234b
vladislav-99 <v.zubov@leviossa.net>
2022-01-25 13:14:18 +0300

    feat(controllers): added deleting image endpoint

--- app/routes/image.routes.ts
+++ app/routes/image.routes.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express';
 import multer from 'multer';
 import path from 'path';
+import fs from 'fs';
+import HttpException from '../exceptions/http.exception';
 
 const imageUpload = multer({
   storage: multer.diskStorage(
@@ -23,7 +25,6 @@ const router = Router();
 
 // get all projects
 router.post('/image', imageUpload.single('image'), (req, res) => {
-  console.log(req.file);
   res.json({
     img_url: `/image/${req.file?.filename}`
   });
@@ -34,6 +35,20 @@ router.get('/image/:filename', (req, res) => {
   const dirname = path.resolve();
   const fullfilepath = path.join(dirname, 'images/' + filename);
   return res.sendFile(fullfilepath);
+});
+
+router.delete('/image/:filename', (req, res, next) => {
+  try {
+    const { filename } = req.params;
+    const dirname = path.resolve();
+    const fullfilepath = path.join(dirname, 'images/' + filename);
+    fs.unlinkSync(fullfilepath);
+    return res.status(200).json({
+      success: true
+    });
+  } catch (error) {
+    next(new HttpException(400, 'Cannot delete image file'));
+  }
 });
 
 export default router;
